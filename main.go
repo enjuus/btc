052616c9f6466405930f44fd3a9bac7b903c7657
enju <enju@protonmail.com>
2017-12-21 09:23:49 +0100

    added monero

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
     "BTC": "https://ethereumprice.org/btc",
     "ETH": "https://ethereumprice.org",
     "LTC": "https://ethereumprice.org/ltc",
+    "XMR": "https://ethereumprice.org/xmr/",
   }
 
   resp := map[string]string{}
@@ -33,13 +34,16 @@ func main() {
   blue := color.New(color.FgBlue).SprintFunc()
   red := color.New(color.FgRed).SprintFunc()
   yellow := color.New(color.FgYellow).SprintFunc()
+  green := color.New(color.FgGreen).SprintFunc()
 
   for k, v := range links {
     resp[k] = GetData(v)
   }
+
   fmt.Printf("%s: %s\n", blue("BTC"), resp["BTC"])
   fmt.Printf("%s: %s\n", red("ETH"), resp["ETH"])
   fmt.Printf("%s: %s\n", yellow("LTC"), resp["LTC"])
+  fmt.Printf("%s: %s\n", green("XMR"), resp["XMR"])
 
 }
 
